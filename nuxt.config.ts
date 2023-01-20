381697c7680f0f873b03fdd3e97ac38e5af01b9a
maruf12 <maruf.harsono@gmail.com>
2023-01-20 21:14:16 +0700

    Add pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@kevinmarrec/nuxt-pwa"],
-  pwa: { workbox: { enabled: true } },
+  pwa: {
+    workbox: { enabled: true },
+    meta: {
+      title: "Digiteam 2.0",
+      author: "JDS",
+    },
+    manifest: {
+      name: "Digiteam 2.0",
+      short_name: "Digiteam PWA",
+    },
+  },
 });
