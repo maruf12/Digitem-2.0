8d8ac819e43742875441bca8eaddbed7cd373de4
maruf12 <maruf.harsono@gmail.com>
2023-01-20 21:52:07 +0700

    Add config meta user-scalable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@kevinmarrec/nuxt-pwa"],
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0",
+        },
+      ],
+    },
+  },
   pwa: {
     workbox: { enabled: true },
     meta: {
