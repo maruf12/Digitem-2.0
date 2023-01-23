060bcf9c3dd4bd3608dec3873770b8c7ab021f0b
sheilaazhar <sheila.azhar98@gmail.com>
2023-01-23 15:37:43 +0700

    create default layouts

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="max-w-5xl min-h-screen mx-auto shadow-md bg-gray-200">
+    <TheHeader />
+    <slot />
+  </div>
+</template>
