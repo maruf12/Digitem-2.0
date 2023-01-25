6e51bed1d869bb42d15a2f0091c40cafa7e31f3b
sheilaazhar <sheila.azhar98@gmail.com>
2023-01-25 10:19:44 +0700

    create user component

---
+++ components/UserProfile/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtLink to="/">
+    <BaseContainer
+      class="bg-gray-900 rounded-lg text-center items-center mt-4 py-5 px-5"
+    >
+      <div class="flex items-center">
+        <img src="/user.svg" class="h-12 mr-3" alt="User Icon" />
+        <div class="text-left flex-grow">
+          <p class="text-sm md:text-base leading-normal font-medium text-white">
+            Sheila Azhar Almufarida
+          </p>
+          <p class="text-xs md:text-sm text-white">
+            Junior Software Programmer - Frontend Engineer
+          </p>
+        </div>
+      </div>
+    </BaseContainer>
+  </NuxtLink>
+</template>
+
+<script setup>
+defineComponent({
+  name: "UserProfile",
+});
+</script>
