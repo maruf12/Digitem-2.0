78283a4981fa9df6c6b2da320c72633fe18d2033
sheilaazhar <sheila.azhar98@gmail.com>
2023-01-25 10:20:55 +0700

    update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <BaseContainer>
-    <p>DigiTeam</p>
+    <UserProfile />
+    <h3 class="text-md text-white font-semibold mt-3">Pengumuman Penting</h3>
+    <Pengumuman />
+    <h3 class="text-md text-white font-semibold mt-3">Artikel</h3>
+    <Artikel />
   </BaseContainer>
 </template>
