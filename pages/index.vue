bbc2ab965f8c17778c484eaf02503ea29987d07d
sheilaazhar <sheila.azhar98@gmail.com>
2023-02-03 12:19:21 +0700

    update card size

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <BaseCard class="items-center mt-8">
+  <BaseCard class="items-center h-screen">
     <h2 class="text-lg text-center font-semibold">Hello World!</h2>
     <NuxtLink to="/login"> <BaseButton>Login</BaseButton> </NuxtLink>
   </BaseCard>
