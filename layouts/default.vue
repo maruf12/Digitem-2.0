9110638c3d0f53edcd2051f09b83bc9c652fb88a
sheilaazhar <sheila.azhar98@gmail.com>
2023-02-03 12:18:40 +0700

    update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="bg-gray-100">
-    <TheHeader />
-    <div class="max-w-lg min-h-screen mx-auto pb-5">
-      <slot />
-    </div>
-  </div>
+  <TheHeader />
+  <main class="max-w-lg min-h-screen mx-auto my-0 pb-5">
+    <slot />
+  </main>
 </template>
