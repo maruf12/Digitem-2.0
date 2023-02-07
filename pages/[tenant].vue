4409611b85c64da0a96f65e65c0afcca441a4054
sheilaazhar <sheila.azhar98@gmail.com>
2023-02-07 16:45:51 +0700

    update: fix tenant page

---
+++ pages/[tenant].vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <UserProfile />
+  <h3 class="text-md font-semibold mt-3 mb-2">Pengumuman Penting</h3>
+  <Pengumuman />
+  <h3 class="text-md font-semibold mt-3 mb-2">Artikel</h3>
+  <Artikel />
+  <h3 class="text-md font-semibold mt-5 mb-2">Data Logistik</h3>
+  <card-item />
+</template>
