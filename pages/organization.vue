4b84f945f951c50e493c1fe5305a58cbaed15647
sheilaazhar <sheila.azhar98@gmail.com>
2023-02-07 16:39:28 +0700

    create organization page

---
+++ pages/organization.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <BaseCard class="items-center h-screen">
+    <h2 class="text-lg text-center font-semibold mb-2">
+      Select Your Organization
+    </h2>
+    <TenantList />
+  </BaseCard>
+</template>
