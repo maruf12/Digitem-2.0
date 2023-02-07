fa458625095bfa7c1c82fde55a5ff75a93fc345b
sheilaazhar <sheila.azhar98@gmail.com>
2023-02-07 16:40:08 +0700

    add list of tenant button

---
+++ components/Tenant/list.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-for="tenant in listTenant" :key="tenant.id">
+    <NuxtLink :to="tenant.name">
+      <BaseButton>{{ tenant.name }}</BaseButton>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+defineComponent({
+  name: "TenantList",
+});
+
+const listTenant = [
+  { id: 1, name: "Jabar Digital Service" },
+  { id: 2, name: "Diskominfo Jabar" },
+];
+</script>
