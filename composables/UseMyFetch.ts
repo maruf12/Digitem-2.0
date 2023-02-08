680bbc7073f4954254419f76a71d1101a36f461b
maruf12 <maruf.harsono@gmail.com>
2023-02-08 22:48:11 +0700

    create composable for fetching data

---
+++ composables/UseMyFetch.ts
@@ -0,0 +1,12 @@
+export const useMyFetch = (request: any, opts: any) => {
+  const config = useRuntimeConfig();
+  const authStore = useAuthStore();
+  const requestOptions = {
+    baseURL: config.public.logisticAPI,
+    ...opts,
+  };
+  if (localStorage.getItem("token")) {
+    requestOptions.headers = { Authorization: `Bearer ${authStore.token}` };
+  }
+  return $fetch(request, requestOptions);
+};
