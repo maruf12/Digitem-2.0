eb80d8c6b3537dcc2cb1266bf676c6a23f31de92
maruf12 <maruf.harsono@gmail.com>
2023-02-08 23:31:31 +0700

    fixing navigateTo with router push

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,9 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to) => {
+  const router = useRouter();
   const publicPages = ["/login", "/"];
   const authRequired = !publicPages.includes(to.path);
   if (authRequired && !localStorage.getItem("token")) {
-    return navigateTo("/login", { replace: true });
+    // return navigateTo("/login", { replace: true });
+    return router.push("/login");
   } else if (localStorage.getItem("token") && to.path === "/login") {
-    return navigateTo("/tenant", { replace: true });
+    // return navigateTo("/tenant", { replace: true });
+    return router.push("/tenant");
   }
 });
