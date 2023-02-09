845bf958d5279f838046c58eb9c2db508e4fe60e
maruf12 <maruf.harsono@gmail.com>
2023-02-09 10:59:05 +0700

    fixing auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,13 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-  const router = useRouter();
-  const authStore = useAuthStore();
   const publicPages = ["/login", "/"];
   const authRequired = !publicPages.includes(to.path);
-  if (authRequired && !authStore.token) {
-    // return navigateTo("/login", { replace: true });
-    return router.push("/login");
-  } else if (authStore.token && to.path === "/login") {
-    // return navigateTo("/tenant", { replace: true });
-    return router.push("/tenant");
+  if (authRequired && !localStorage.getItem("token")) {
+    return navigateTo("/login", { replace: true });
+  } else if (localStorage.getItem("token") && to.path === "/login") {
+    return navigateTo("/tenant", { replace: true });
   }
 });
