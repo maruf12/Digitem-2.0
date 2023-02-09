ee4a15c4c07680add94fa1c87603194718f6cca3
sheilaazhar <sheila.azhar98@gmail.com>
2023-02-09 13:26:02 +0700

    fix layout

--- pages/[tenant].vue
+++ pages/[tenant].vue
@@ -1,34 +1,22 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-<<<<<<< HEAD:pages/[tenant].vue
   <UserProfile />
   <h3 class="text-md font-semibold mt-3 mb-2 mx-3">Pengumuman Penting</h3>
   <Pengumuman />
   <h3 class="text-md font-semibold mt-3 mb-2 mx-3">Artikel</h3>
   <Artikel />
   <h3 class="text-md font-semibold mt-3 mb-2 mx-3">Data Logistik</h3>
-  <card-item />
-=======
-  <BaseContainer class="mb-10">
-    <UserProfile />
-    <h3 class="text-md text-white font-semibold mt-3">Pengumuman Penting</h3>
-    <Pengumuman />
-    <h3 class="text-md text-white font-semibold mt-3">Artikel</h3>
-    <Artikel />
-    <h3 class="text-md text-white font-semibold mt-5">Data Logistik</h3>
-    <!-- <card-item /> -->
-    <BaseButton @click="getMe">Get me</BaseButton>
-    <p>{{ user.name }}</p>
-  </BaseContainer>
->>>>>>> main:pages/tenant.vue
+  <!-- <card-item /> -->
+  <BaseButton class="ml-3" @click="getUserMe">Get me</BaseButton>
+  <p class="ml-3">{{ userInfo.name }}</p>
 </template>
 <script setup lang="ts">
-const user = ref("");
-const getMe = async () => {
+const userInfo:any = ref("");
+const getUserMe = async () => {
   await useMyFetch("/me", {
     method: "GET",
-  }).then((response) => {
-    user.value = response.data;
+  }).then((response:any) => {
+    userInfo.value = response.data;
   });
 };
 </script>
