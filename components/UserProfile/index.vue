0f806f0b841d4e9142cdd756fa792bffdc74c5a9
sheilaazhar <sheila.azhar98@gmail.com>
2023-02-10 14:58:46 +0700

    dynamic name and organization

--- components/UserProfile/index.vue
+++ components/UserProfile/index.vue
@@ -5,10 +5,10 @@
         <img src="/user.svg" class="h-12 mr-3" alt="User Icon" />
         <div class="text-left flex-grow">
           <p class="text-sm md:text-base leading-normal font-medium">
-            Sheila Azhar Almufarida
+            {{ user.name }}
           </p>
           <p class="text-xs md:text-sm">
-            {{ $route.params.tenant }}
+            {{ organization.name }}
           </p>
         </div>
       </div>
@@ -20,4 +20,6 @@
 defineComponent({
   name: "UserProfile",
 });
+const user = JSON.parse(localStorage.getItem('user'));
+const organization = JSON.parse(localStorage.getItem('organization'));
 </script>
