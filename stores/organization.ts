f961fba864bd3116830e829f3eb9b5cb09d28c6c
maruf12 <maruf.harsono@gmail.com>
2023-02-16 13:51:46 +0700

    fixing eslint error

--- stores/organization.ts
+++ stores/organization.ts
@@ -1,12 +1,11 @@
 export const useOrganizationStore = defineStore("organization", {
-    state: () => ({
-      organization: JSON.parse(localStorage.getItem("organization") || "{}"),
-    }),
-    actions: {
-      toOrganization(tenant:any) {
-        this.organization = tenant;
-        localStorage.setItem("organization", JSON.stringify(this.organization));
-      },
+  state: () => ({
+    organization: JSON.parse(localStorage.getItem("organization") || "{}"),
+  }),
+  actions: {
+    toOrganization(tenant: any) {
+      this.organization = tenant;
+      localStorage.setItem("organization", JSON.stringify(this.organization));
     },
-  });
-  
+  },
+});
